12eb56f5994bc83c037cf9e28fc97da5f8c02e84
EC2 Default User <ec2-user@ip-172-31-20-228.ap-northeast-1.compute.internal>
2023-08-07 11:02:50 +0000

    [Finish]2-5

---
+++ practice/apple.rb
@@ -0,0 +1,9 @@
+apple = "nagano"
+
+if apple == "aomori"
+    puts "青森産"
+elsif apple == "nagano"
+    puts "長野産"
+else
+    puts "somewhere"
+end
---
+++ xercise/lesson5.rb
@@ -0,0 +1,9 @@
+total_price = 150
+
+if total_price > 100
+    puts "みかんを購入、所持金余りあり"
+elsif total_price == 100
+    puts "みかんを購入、所持金余りなし"
+else
+    puts "みかんを購入できません"
+end
